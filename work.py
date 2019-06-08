337e4a887a7a243a7c1085f90e18176a5bf568d6
carlos_hdc <carlos_hdc@hotmail.com>
2019-06-07 21:47:11 -0300

    rk2 testado com exemplos da web, falta acertar as equações

--- work.py
+++ work.py
@@ -1,26 +1,35 @@
-alfa = 0.2  #quantidade de tempo suscetivel a contaminação
-beta = 0.1  #chance de reinfecção do meme
-gamma = 0.1  #velocidade da perda de interesse
+import math
 
-def eq1(t, values):
+alfa = 5.78      #quantidade de tempo suscetivel a contaminação
+beta = 3.91      #chance de reinfecção do meme
+gamma = 1.26     #velocidade da perda de interesse
+
+def healthy(t, values):
+	global alfa
+	# print((-alfa)*(values[0]*values[1]))
 	return (-alfa)*(values[0]*values[1])
 
-def eq2(t, values):
+def infected(t, values):
+	global alfa,beta,gamma
+	# print(((alfa*values[0]*values[1]) + (beta*values[1]*values[2]) + (-gamma*values[1])))
 	return ((alfa*values[0]*values[1]) + (beta*values[1]*values[2]) + (-gamma*values[1]))
 
-def eq3(t, values):
+def clean(t, values):
+	global beta,gamma
+	# print(((-beta*values[1]*values[2]) + (gamma*values[1])))
 	return ((-beta*values[1]*values[2]) + (gamma*values[1]))
+	
 
-def rk2(values, t=0, h=0.1, iterations=2):
+def rk2(values, t=0, h=0.1, iterations=3):
 	aux = [0,0,0]
 	for _ in range(iterations):
 		tp = t + h
 		
-		k1 =  [eq1(t,values),eq2(t,values),eq3(t,values)]
+		k1 =  [healthy(t,values),infected(t,values),clean(t,values)]
 		
 		aux = [values[0]+(h*k1[0]), values[1]+(h*k1[1]), values[2]+(h*k1[2])]
 		
-		k2 = [eq1(tp,aux), eq2(tp,aux), eq3(tp,aux)]
+		k2 = [healthy(tp,aux), infected(tp,aux), clean(tp,aux)]
 		
 		values[0] = values[0] + (h/2)*(k1[0]+k2[0])
 		values[1] = values[1] + (h/2)*(k1[1]+k2[1])
@@ -28,4 +37,4 @@ def rk2(values, t=0, h=0.1, iterations=2):
 
 		print(values)
 
-rk2([1000,10,0])
+rk2([1,0,100])
