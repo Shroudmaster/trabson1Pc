50e43d789ede9bc1fd220ed450bb02b6f20efd33
carlos_hdc <carlos_hdc@hotmail.com>
2019-06-07 21:17:00 -0300

    testes iniciais do rk2

--- work.py
+++ work.py
@@ -1,9 +1,9 @@
-alfa = 0.1
-beta = 0.1
-gamma = 0.1
+alfa = 0.2  #quantidade de tempo suscetivel a contaminação
+beta = 0.1  #chance de reinfecção do meme
+gamma = 0.1  #velocidade da perda de interesse
 
 def eq1(t, values):
-	return -alfa*(values[0]*values[1])
+	return (-alfa)*(values[0]*values[1])
 
 def eq2(t, values):
 	return ((alfa*values[0]*values[1]) + (beta*values[1]*values[2]) + (-gamma*values[1]))
@@ -22,6 +22,10 @@ def rk2(values, t=0, h=0.1, iterations=2):
 		
 		k2 = [eq1(tp,aux), eq2(tp,aux), eq3(tp,aux)]
 		
-		values[0] = values[0] + (h/2)*(k1+k2)
-		values[1] = values[1] + (h/2)*(k1+k2)
-		values[2] = values[2] + (h/2)*(k1+k2)
+		values[0] = values[0] + (h/2)*(k1[0]+k2[0])
+		values[1] = values[1] + (h/2)*(k1[1]+k2[1])
+		values[2] = values[2] + (h/2)*(k1[2]+k2[2])
+
+		print(values)
+
+rk2([1000,10,0])
